2fd152db14b2a23984044a58cff8c3e32062b5eb
chukwuka promise uba <promiseuba67@gmail.com>
2023-12-19 02:12:02 +0100

    first Commit

---
+++ src/config/index.ts
@@ -0,0 +1,28 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "Ui kits",
+    value: "ui_kits" as const,
+    featured: [
+      { name: "Editor picks", href: "#", imageSrc: "/nav/ui-kits/mixed.jpg" },
+      { name: "New Arrivals", href: "#", imageSrc: "/nav/ui-kits/blue.jpg" },
+      { name: "Bestsellers", href: "#", imageSrc: "/nav/ui-kits/purple.jpg" },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "favorite icon picks",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      { name: "New Arrivals", href: "#", imageSrc: "/nav/icons/new.jpg" },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
